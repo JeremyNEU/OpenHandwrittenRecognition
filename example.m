b3e71d1de2993f173743afa115fdfdd49fe98ed3
Zhenqiang.Ying <yingzhenqiang@gmail.com>
2015-10-18 22:41:22 +0800

    数据可视化

--- example.m
+++ example.m
@@ -1,3 +1,14 @@
+%% output images for testing feature extraction 
+data = loadMNISTImages('dataset/mnist/train-images.idx3-ubyte');
+labels = loadMNISTLabels('dataset/mnist/train-labels.idx1-ubyte');
+
+N = length(data);
+img = reshape(data,28,28,N);
+
+% for idx = 1:40
+%     imwrite(img(:,:,idx),num2str(labels(idx), '%d.jpg'));
+% end
+
 meshSize = [5,3];
 blockSize = [6,10];
 binaryFunc = @(img)(im2bw(img, graythresh(img))); %Otsu
@@ -6,4 +17,12 @@ featExtrFunc = @(img)(extractCoarseMeshFeatures(binaryFunc(img), ...
 featSize = meshSize(1)*meshSize(2);
 
 load('Model.mat');
-testModel(Model, featExtrFunc, featSize);
+confMat = testModel(Model, featExtrFunc, featSize);
+
+
+trueRecog = diag(confMat);
+falseRecog = confMat.*~eye(10); % or confMat - diag(trueRecog)
+
+figure;
+subplot(121);imshow(mat2gray(falseRecog));
+subplot(122);bar(0:9,trueRecog);
--- showModel.m
+++ showModel.m
@@ -4,3 +4,16 @@ for idx = 1:10
     pattern{idx} = reshape(Model.F(:,idx),meshSize);
 end
 implot(pattern{:});
+
+for idx = 1:10
+    subplot(3,4,idx);title(num2str(idx-1));
+end
+
+figure;
+for idx = 1:10
+    subplot(3,4,idx);
+    I = mat2gray(pattern{idx});
+    BW = im2bw(I, 0.4);
+    imshow(BW);
+    title(num2str(idx-1));
+end
